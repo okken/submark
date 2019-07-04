b8449654e07d69f6f7acde04d939e7cdce569afb
Brian Okken <1568356+okken@users.noreply.github.com>
2019-07-03 19:45:14 -0700

    inital script

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # submark
-A Subset of Markdown
+Subset of Markdown
+
+## Features
+
+* Convert headings 
+    * `# heading`  -> `<h1> heading </h1>`
+    * `## heading`  -> `<h2> heading </h2>`
+    * `### heading`  -> `<h3> heading </h3>`
+    * `#### heading`  -> `<h4> heading </h4>`
+    * `##### heading`  -> `<h5> heading </h5>`
+
+* Convert strong
+    * `**strong**` -> `<strong>strong</strong>`
+    * `__strong__`-> `<strong>strong</strong>`
+
+
+* Convert em
+    * `**something**` -> `<em>something</em>`
+    * `__something__`-> `<em>something</em>`
+
+
+## Usage
+
+Use it like this:
+~~~
+$ python3 submark.py something.md > something.html
+~~~
+
+or:
+~~~
+$ cat something.html | python3 submark.py > something.html
+~~~
+
+or:
+~~~
+$ echo '**hi**' | python3 submark.py
+<strong>hi</strong>
+~~~
+
+
+## Testing
+
+Test in a virtual environment with the usual incantations.
+
+~~~
+$ git clone https://github.com/okken/submark.git
+$ cd submark
+$ python3 -m venv venv --prompt submark
+$ source venv/bin/activate
+(submark) $ pip install -U pip
+~~~
+
+Then install `pytest` and `pytest-cov`.
+
+~~~
+(submark) $ pip install pytest pytest-cov
+~~~
+
+There's already a `.coveragerc` file to exclude `venv` or `.venv`.
+
+Now run tests.
+
+~~~
+(submark) $ pip install pytest pytest-cov
+(submark) $ pytest --cov=.
+~~~
+
+
+## History
+
+* 0.1 Initial script and tests.
+
---
+++ submark.py
@@ -0,0 +1,64 @@
+"""
+A Subset of Markdown
+
+Use it like this:
+python3 submark.py something.md > something.html
+
+or:
+cat something.html | python3 submark.py > something.html
+
+or:
+$ echo '**hi**' | python3 submark.py
+<strong>hi</strong>
+"""
+
+__version__ = '0.1'
+
+import fileinput
+import re
+
+
+def headers(line):
+    line = re.sub(r"^# (.*)", r"<h1> \1 </h1>", line)
+    line = re.sub(r"^## (.*)", r"<h2> \1 </h2>", line)
+    line = re.sub(r"^### (.*)", r"<h3> \1 </h3>", line)
+    line = re.sub(r"^#### (.*)", r"<h4> \1 </h4>", line)
+    line = re.sub(r"^##### (.*)", r"<h5> \1 </h5>", line)
+    return line
+
+
+def strong(line):
+    line = re.sub(r"\*\*(.*)\*\*", r"<strong>\1</strong>", line)
+    line = re.sub(r"__(.*)__", r"<strong>\1</strong>", line)
+    return line
+
+
+def em(line):
+    line = re.sub(r"\*(.*)\*", r"<em>\1</em>", line)
+    line = re.sub(r"_(.*)_", r"<em>\1</em>", line)
+    return line
+
+
+def convert_line(line):
+    line = line.rstrip()
+    line = headers(line)
+    line = strong(line)
+    line = em(line)
+    return line
+
+
+def convert_list(lines):
+    converted_list = [convert_line(l) for l in lines]
+    return "\n".join(converted_list)
+
+
+def convert(some_string):
+    return convert_list(some_string.split("\n"))
+
+
+def main():
+    print(convert_list(fileinput.input()))
+
+
+if __name__ == "__main__":
+    main()
